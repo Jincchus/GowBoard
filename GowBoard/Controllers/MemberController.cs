1eecf51680420d7f1c6f738a40e6a4445a94f6e8
Jincchus <chopoo2001@gmail.com>
2024-06-19 16:46:09 +0900

    20240619 solution structure

--- GowBoard/Controllers/MemberController.cs
+++ GowBoard/Controllers/MemberController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GowBoard.Models;
+using GowBoard.Models.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace GowBoard.Controllers
 {
     public class MemberController : Controller
     {
+        private MemberDbContext db = new MemberDbContext();
+
         // GET: Member
         public ActionResult Index()
         {
@@ -21,12 +25,43 @@ namespace GowBoard.Controllers
             return View();
         }
 
+        // Post: Member/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Member member)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Member.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("LogIn", "Member");
+            }
+
+            return View(member); 
+        }
+
+
         // GET: Member/LogIn
         // 로그인
         public ActionResult LogIn() 
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(Member member) 
+        {
+            var logInMember = db.Member.FirstOrDefault(u => u.MemberId == member.MemberId && u.Password == member.Password);
+            if (logInMember != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid username or password");
+            return View(member);
+        }
+
 
         // GET: Member/FindId
         // 아이디 찾기
---
+++ GowBoard/Models/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GowBoard.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoleId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string RoleName { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
