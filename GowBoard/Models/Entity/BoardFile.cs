a506c54ae82b4cd67d3d1bae340b06adcffd0fa3
Jincchus <chopoo2001@gmail.com>
2024-06-19 18:01:50 +0900

    20240619

---
+++ GowBoard/Models/Entity/BoardFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GowBoard.Models.Entity
+{
+    public class BoardFile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BoardFileId { get; set; }
+
+        [Required]
+        public int BoardContentId { get; set;}
+
+        [Required]
+        public String FileData { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public int FileSize { get; set; }
+
+        [Required]
+        public string FileMimeType { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ GowBoard/Models/Entity/MemberRoleMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GowBoard.Models.Entity
+{
+    public class MemberRoleMap
+    {
+        [Key]
+        [Column(Order = 1)]
+        [StringLength(50)]
+        public string MemberId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int RoleId { get; set; }
+
+        public Member Member { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
