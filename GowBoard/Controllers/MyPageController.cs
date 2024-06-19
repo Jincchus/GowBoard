4a1a32a6b0faee2f50a5f097f06e34de70a4e36f
Jincchus <chopoo2001@gmail.com>
2024-06-19 17:29:36 +0900

    merge

--- GowBoard/Controllers/MyPageController.cs
+++ GowBoard/Controllers/MyPageController.cs
@@ -49,4 +49,55 @@ namespace GowBoard.Controllers
             return View(); 
         }
     }
+}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GowBoard.Controllers
+{
+    public class MyPageController : Controller
+    {
+        // GET: MyPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MyPage/Profile
+        // 프로필 페이지
+        public ActionResult Profile() 
+        {
+            return View();
+        }
+
+        // Get: MyPage/UpdateProfile
+        // 프로필 수정 페이지
+        public ActionResult UpdateProfile() 
+        {
+            return View();
+        }
+
+        // Get: MyPage/MyCommentList
+        // 내가 쓴 댓글 리스트 페이지
+        public ActionResult MyCommentList() 
+        {
+            return View();
+        }
+
+        // Get: MyPage/MyPostList
+        // 내가 쓴 게시글 리스트 페이지
+        public ActionResult MyPostList()
+        { 
+            return View();
+        }
+
+        // Get: MyPage/Withdrawal
+        // 회원 탈퇴 페이지
+        public ActionResult Withdrawal() 
+        {
+            return View(); 
+        }
+    }
 }
---
+++ GowBoard/Models/Member.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GowBoard.Models
+{
+    public class Member
+    {
+        [Key]
+        [Required]
+        [StringLength(50)]
+        public string MemberId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nickname { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public bool DeleteYn { get; set; } = false;
+
+        public DateTime? DeletedAt { get; set; }
+    }
+}
