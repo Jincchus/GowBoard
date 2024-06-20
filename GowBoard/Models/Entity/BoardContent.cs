2df6db3bd5df05aacd4e93200dc7ed86960f87a5
Jincchus <chopoo2001@gmail.com>
2024-06-20 09:02:41 +0900

    soulution

---
+++ GowBoard/Models/Entity/BoardContent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GowBoard.Models.Entity
+{
+    public class BoardContent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BoardContentId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Category { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [ForeignKey("member_id")]
+        public string WriterId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+
+        public int ViewCount { get; set; } = 0;
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+
+
+        public DateTime? ModifiedAt { get; set; }
+
+
+        public string ModifierId { get; set; }
+
+        [ForeignKey("member_id")]
+        public virtual Member Member { get; set; }
+    }
+}
